dd26bc5d65209478ac73aaff5f867c3c151a8153
Nils Werner <nils.werner@gmail.com>
2010-11-01 13:54:54 +0100

    German localization

---
+++ lang/lang.de.php
@@ -0,0 +1,31 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Nils Werner',
+			'email' => 'nils.werner@gmail.com',
+			'website' => 'http://www.obsessive-media.de'
+		),
+		'release-date' => '2010-11-01'
+	);
+	
+	
+	/*
+	 * EXTENSION: Show/Hide Fields
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+		"Collapse" =>
+		"Einklappen",
+		
+		"Collapse all" =>
+		"Alle einklappen",
+		
+		"Expand" =>
+		"Ausklappen",
+		
+		"Expand all" =>
+		"Alle ausklappen"
+	);
